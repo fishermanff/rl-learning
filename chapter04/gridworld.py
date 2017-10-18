b0d7f442a4a15b26be8a0b7cd20e5f2c86ef54b1
chenjianfeng02 <chenjianfeng02@meituan.com>
2017-10-18 18:55:23 +0800

    add policy iteration

--- chapter04/gridworld.py
+++ chapter04/gridworld.py
@@ -65,10 +65,74 @@ class GridWorld(object):
             k += 1
         self.grid = self.grid.round(1)
 
+    # policy iteration (using iterative policy evalution)
+    def policyIteration(self, epoch=20, discount=1.0):
+        k = 0
+        self.grid = np.zeros((self.gridHeight, self.gridWidth))
+        while k<epoch:
+            if(k in [0, 1, 2, 3]):
+                print("k =", k)
+                self.printValueGraph()
+                self.printPolicyGraph()
+            # policy evaluation
+            while True:
+                newGrid = np.zeros((self.gridHeight, self.gridWidth))
+                for i in range(self.gridHeight):
+                    for j in range(self.gridWidth):
+                        if((i,j) not in self.terminals):
+                            for action in self.actions:
+                                newGrid[i,j] += self.actionProbOfState[i,j][action] * (self.rewardOfStateAction[i,j][action] + \
+                                                        discount * self.grid[self.successorOfStateAction[i,j][action]])
+                if(np.sum(np.abs(self.grid - newGrid)) < 1e-4):
+                    self.grid = newGrid
+                    break
+                self.grid = newGrid
+
+            # policy improvement
+            isPolicyStable = True
+            for i in range(self.gridHeight):
+                for j in range(self.gridWidth):
+                    oldActionProb = self.actionProbOfState[i,j]
+                    actionValue = []
+                    for action in self.actions:
+                        actionValue.append((self.rewardOfStateAction[i,j][action] + \
+                                                    discount * self.grid[self.successorOfStateAction[i,j][action]]))
+                        mask = [1 if val==max(actionValue) else 0 for val in actionValue]
+                        newProb = [1.0/sum(mask) if bit==1 else 0.0 for bit in mask]
+                    self.actionProbOfState[i,j] = dict(zip(self.actions, newProb))
+                    if(oldActionProb!=self.actionProbOfState[i,j]):
+                        isPolicyStable = False
+            if(isPolicyStable):
+                return self.grid, self.actionProbOfState
+            k += 1
+
+    def printPolicyGraph(self):
+        policyOfState = []
+        for i in range(self.gridHeight):
+            policyOfOneRow = []
+            for j in range(self.gridWidth):
+                if((i,j) in self.terminals):
+                    policyOfOneRow.append(["#", "#", "#", "#"])
+                else:
+                    policyOfOneRow.append([action if self.actionProbOfState[i,j][action]>0. else '#' for action in self.actions])
+            policyOfState.append(policyOfOneRow)
+        for row in range(self.gridHeight):
+            print(policyOfState[row])
+
+    def printValueGraph(self):
+        print(self.grid.round(1))
+
 if __name__ == '__main__':
     gridWorld = GridWorld()
+    print("\nrandom policy (fig4.1 left)...")
     gridWorld.calculateRandomGrid()
     print("random policy grid: ")
-    print(gridWorld.grid)
+    gridWorld.printValueGraph()
 
+    print("\npolicy iteration (fig4.1 right)...")
+    optimalValue, optimalPolicy = gridWorld.policyIteration()
+    print("optimal grid: ")
+    gridWorld.printValueGraph()
+    print("optimal policy")
+    gridWorld.printPolicyGraph()
 
